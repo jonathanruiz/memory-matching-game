c9f9a46ccda46b71bc890058d011c73450c97376
Jonathan Ruiz <jonathan.ruiz1196@gmail.com>
2018-07-02 15:44:12 -0600

    feat: added playGame function

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ let generateStars = (starTwo, starThree) => {
   return `<li><i class="fa fa-star"></i></li> 
           <li><i class="fa fa-star${starTwo}"></i></li> 
           <li><i class="fa fa-star${starThree}"></i></li>`;
-}
+};
 
 // Congratulations Modal
 let modal = () => {
@@ -48,6 +48,7 @@ let timer = new Timer();
 let numStars = 3;
 let match = 0;
 let moves = 0;
+let openCards = [];
 
 // Query Selectors for classes
 const deck = document.querySelector(".deck");
@@ -80,10 +81,45 @@ let shuffle = array => {
   return array;
 };
 
+// Function that allows you to plays the game
+let playGame = () => {
+  const allCards = document.querySelectorAll(".card");
+
+  allCards.forEach(card => {
+    card.addEventListener("click", () => {
+      // When clicked, add the card to the array of open cards and flip the card to show it
+      if (
+        !card.classList.contains("open") &&
+        !card.classList.contains("show") &&
+        !card.classList.contains("match")
+      ) {
+        openCards.push(card);
+        card.classList.add("open", "show");
+
+        // Flips the cards
+        flipCards();
+
+        if (moves <= 10) {
+        } else if (moves <= 14) {
+          numStars = 2;
+          stars.innerHTML = generateStars("", "-o");
+        } else if (moves <= 18) {
+          numStars = 1;
+          stars.innerHTML = generateStars("-o", "-o");
+        }
+
+        // If the game is won, show the congratulations screen
+        if (match == 8) {
+          modal();
+        }
+      }
+    });
+  });
+};
+
 // Function that starts the game
 let startGame = () => {
-
-  // Randomly creates an HTML element for each card, using shuffle function.
+  // Randomly creates an HTML element for each card, using shuffle function
   let cardHTML = shuffle(cards).map(card => {
     return generateCard(card);
   });
@@ -93,9 +129,10 @@ let startGame = () => {
   moves = 0;
   moveCounter.innerHTML = moves;
 
-  // Start timer and display it on the screen
+  // Resets and starts the timer then display it on the screen
+  timer.reset();
   timer.start();
-  timer.addEventListener('secondsUpdated', (e) => {
+  timer.addEventListener("secondsUpdated", e => {
     timerClock.innerHTML = timer.getTimeValues().toString();
   });
 
@@ -104,6 +141,8 @@ let startGame = () => {
 
   // Join the HTML elements together
   deck.innerHTML = cardHTML.join("");
+
+  playGame();
 };
 
 // Function that flips the cards
@@ -154,43 +193,6 @@ startGame();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
-const allCards = document.querySelectorAll(".card");
-let openCards = [];
-
-allCards.forEach(card => {
-  card.addEventListener("click", () => {
-    // When clicked, add the card to the array of open cards and flip the card to show it
-    if (
-      !card.classList.contains("open") &&
-      !card.classList.contains("show") &&
-      !card.classList.contains("match")
-    ) {
-      openCards.push(card);
-      card.classList.add("open", "show");
-
-      // Flips the cards
-      flipCards();
-
-      if (moves <= 10) {
-    
-      }
-      else if (moves <= 14) {
-        numStars = 2;
-        stars.innerHTML = generateStars("", "-o");
-      }
-      else if (moves <= 18) {
-        numStars = 1;
-        stars.innerHTML = generateStars("-o", "-o");
-      }
-
-      // If the game is won, show the congratulations screen
-      if (match == 8) {
-        modal();
-      }
-    }
-  });
-});
 
 // Restarts the game when called
 restartButton.addEventListener("click", () => {
